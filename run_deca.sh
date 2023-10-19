095d701e0a66c746289df37d1531d0752af9fd02
Yongqing Liang at gvc-3090 <root@lyq.me>
2023-10-19 16:35:59 -0500

    add run script

---
+++ run_deca.sh
@@ -0,0 +1,8 @@
+python demos/demo_reconstruct.py \
+    --saveDepth True \
+    --saveObj True \
+    --rasterizer_type=pytorch3d \
+    --useTex False
+    -i TestSamples/examples \
+    -s TestSamples/examples
+    
